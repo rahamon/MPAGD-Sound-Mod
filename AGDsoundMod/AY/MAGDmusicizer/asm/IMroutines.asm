e9de9cf54257bfbeb15b35da5ae40adb2b6779b3
Ramón Fernández Ameijeiras <76912327+rahamon@users.noreply.github.com>
2021-09-07 11:41:33 +0200

    Update IMroutines.asm

--- AGDsoundMod/AY/MAGDmusicizer/asm/IMroutines.asm
+++ AGDsoundMod/AY/MAGDmusicizer/asm/IMroutines.asm
@@ -1,12 +1,5 @@
 ; ---------------------------------------------------------------------
 ;	IMsubroutines
-;	Set LoopOff with SOUND 0
-;	Set SOUND to the music you want to play eg SOUND 1 - 29.
-;	When the music starts control will then be set to 30!! This means music is playing
-;	To stop and mute the music set SOUND 31
-;	To halt (SKIP) playback SOUND 32 (note this does not mute the AY chip)
-;   Monitor con SOUND 36
-;   Fx con SOUND > 100
 ; ---------------------------------------------------------------------
 	
 ; interrupt routine itself 
