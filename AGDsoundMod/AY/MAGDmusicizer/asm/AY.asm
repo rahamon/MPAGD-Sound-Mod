d97d457bb79b62bfe89abb06440db2b6c43ce0cd
Ramón Fernández Ameijeiras <76912327+rahamon@users.noreply.github.com>
2021-09-07 12:08:02 +0200

    Update AY.asm

--- AGDsoundMod/AY/MAGDmusicizer/asm/AY.asm
+++ AGDsoundMod/AY/MAGDmusicizer/asm/AY.asm
@@ -3,17 +3,6 @@
 ; -----------------------------------------------------------------------------
 
 isnd
-		;call chksn
-		;ld a,c				; recogemos SOUND n en registro a
-	
-		;ld (ctrN),a			; guardamos n en ctrN
-		; dame un respiro
-		;push ix
-		;ld b,1
-		;call delay
-		;pop ix
-		;ret
-		
 		include "isnd.asm"
 		
 ; mute/pause/stop a ISRsound.
@@ -29,33 +18,26 @@ silenc
 fxsnd	ret
 
 FXsetup						; inicializacion de drivers
-		;ld hl,sfxBankAd		
-		call bank4			;*
-		ld hl,(FX4BANK)		;*
+		call bank4
+		ld hl,(FX4BANK)
 		call FX4BANK + 2	; AFXINIT setup FX
-		call bank0			;*
+		call bank0
 		ret
-Selectfx					; Inicia efecto
-		;ld a,(sfxBankAd)	; verifique el efecto (c) existe en el banco
+Selectfx					; SELECT efecto
 		call bank4
 		
-		ld a,(ctrF)			; sound to play.
+		ld a,(ctrF)			; efect to play.
 		ld c,a
 		dec c
-		
-		ld hl,(FX4BANK)		;*
-		ld a,(hl)			; verifique el efecto (c) existe en el banco
+		; Chequera FX (c)
+		ld hl,(FX4BANK)
+		ld a,(hl)
 		dec a
-		cp c
+		cp c			; verifique el efecto (c) existe en el banco
 		jr c,playSfx2		; transicion si el banco no tiene tantos efectos
 		ld a,c				; lanzar el efecto (c)
 		call FX4BANK + 5	; AFXPLAY
 playSfx2
-		call bank0			;*
+		call bank0
 		ret
-
-;AYFxPlayer
-;		include "ayfxplay.a80.asm"	; enciende la fuente del reproductor de efectos
-;sfxBankAd
-;		incbin "sfxBank.afb"
 	
