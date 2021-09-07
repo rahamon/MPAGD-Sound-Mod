89e88caa80543d2dc2c4ad27c81bde7ca82f82fa
Ramón Fernández Ameijeiras <76912327+rahamon@users.noreply.github.com>
2021-09-07 15:49:06 +0200

    Update trailer.asm

--- AGDsoundMod/AY/MAGDmusicizer/asm/trailer.asm
+++ AGDsoundMod/AY/MAGDmusicizer/asm/trailer.asm
@@ -1,5 +1,5 @@
 eop		equ $
-		;defs 256,0						; parche: reservar 256 bytes para PUTBLOCK en modo aventura
+		;defs 256,0						; parche: descomentra y reservar 256 bytes para PUTBLOCK en modo aventura
 		if ($%256 != 0)
 			org (($/256)*256)+256				; Align to a 256 byte page boundary
 		endif
