1c21cd1bfd645fdc318bd4d12a00d590b7643153
Ramón Fernández Ameijeiras <76912327+rahamon@users.noreply.github.com>
2021-09-07 15:47:56 +0200

    Update trailer.asm

--- AGDsoundMod/AY/Arkos/trailer.asm
+++ AGDsoundMod/AY/Arkos/trailer.asm
@@ -1,4 +1,4 @@
 eop		equ $
-		;defs 256,0			; parche reserva 256 bytes para PUTBLOCK en modo aventura
+		;defs 256,0			; parche: descomentar para reservar 256 bytes para PUTBLOCK en modo aventura
 		align 256
 IJUMPA	equ $
