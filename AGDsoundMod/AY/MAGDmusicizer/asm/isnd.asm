a3edce200028e5aabcb8bc69ea5930ce0d489a0b
Ramón Fernández Ameijeiras <76912327+rahamon@users.noreply.github.com>
2021-09-07 11:44:14 +0200

    Update isnd.asm

--- AGDsoundMod/AY/MAGDmusicizer/asm/isnd.asm
+++ AGDsoundMod/AY/MAGDmusicizer/asm/isnd.asm
@@ -1,11 +1,4 @@
 ; 	Chequear sonido, comando o efecto
-;	Set LoopOff with SOUND 0
-;	Set SOUND to the music you want to play eg SOUND 1 - 29.
-;	When the music starts control will then be set to 30!! This means music is playing
-;	To stop and mute the music set SILENCE
-;	To halt (SKIP) playback SOUND 32 (note this does not mute the AY chip)
-;	Monitor con SOUND 36
-;	Fx con SOUND > 100
 
 		call div41
 		ld a,c				; recogemos SOUND n en registro a
